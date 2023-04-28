495a78bc802362e2ad6e88bd74b23f900a92e6bb
Karl Rapur <rapurkarl@gmail.com>
2023-04-29 01:22:25 +0300

    Add generated services

--- src/composables/useServices.ts
+++ src/composables/useServices.ts
@@ -1,3 +1,43 @@
+import { Configuration, AuthApi } from '#/auth'
+import { BookingApi } from '#/bookings'
+import { CheckingApi } from '#/checking'
+import { NotificationsApi } from '#/notifications'
+import { PaymentsApi } from '#/payments'
+import { RoomsApi } from '#/rooms'
+import { SupportApi } from '#/support'
+
+export interface ApiServices {
+  authApi: AuthApi
+  bookingApi: BookingApi
+  checkingApi: CheckingApi
+  notificationsApi: NotificationsApi
+  paymentsApi: PaymentsApi
+  roomsApi: RoomsApi
+  supportApi: SupportApi
+}
+
 export function useServices() {
-    
+  const accessToken = localStorage.getItem('jwtToken') || undefined
+
+  const configuration: Configuration = new Configuration({
+    accessToken
+  })
+
+  const authApi: AuthApi = new AuthApi(configuration)
+  const bookingApi: BookingApi = new BookingApi(configuration)
+  const checkingApi: CheckingApi = new CheckingApi(configuration)
+  const notificationsApi: NotificationsApi = new NotificationsApi(configuration)
+  const paymentsApi: PaymentsApi = new PaymentsApi(configuration)
+  const roomsApi: RoomsApi = new RoomsApi(configuration)
+  const supportApi: SupportApi = new SupportApi(configuration)
+
+  return {
+    authApi,
+    bookingApi,
+    checkingApi,
+    notificationsApi,
+    paymentsApi,
+    roomsApi,
+    supportApi
+  }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { useServices } from '@/composables/useServices'
 import TheWelcome from '../components/TheWelcome.vue'
+
+const { roomsApi } = useServices()
+
+roomsApi
+  .getRooms()
+  .then((response) => console.log(response.data))
+  .catch((error) => console.log(error))
 </script>
 
 <template>
