4754cfe51da119c5e4f5f5c27d0a4bbdf1e9b1c8
Laima <laima.anna.d@gmail.com>
2023-05-03 16:14:23 +0300

    change my booking view

--- src/views/MyBookingsView.vue
+++ src/views/MyBookingsView.vue
@@ -29,8 +29,8 @@ const clickDetails = (number:any): void => {
         <div class="" v-for="booking in bookings" :key="booking.id">
           <div class="card m-5 bg-light">
             <div class="card-body">
-              <h5 class="card-title">Room number: {{ booking.roomId }}</h5>
-              <p class="card-text">Price per night: {{ booking.price }} <br /> Start date: {{ booking.startDate }} <br />
+              <h5 class="card-title"> Booking id: {{ booking.id }}</h5>
+              <p class="card-text">Room number: {{ booking.roomId }} <br/> Price per night: {{ booking.price }} <br /> Start date: {{ booking.startDate }} <br />
                 End date: {{ booking.endDate }} <br /> Status: {{ booking.status }}</p>
               <button type="button" @click="clickDetails(booking.id)" class="btn btn-primary">Details</button>
             </div>
