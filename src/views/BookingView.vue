12a0545840468e6c5c3ab6320f8a9120198f534c
Laima <laima.anna.d@gmail.com>
2023-05-03 16:10:22 +0300

    Add room details for a booking

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
 import type { Booking } from '#/bookings'
+import type { Room } from '#/rooms'
 import { useServices } from '@/composables/useServices'
 import { ref, type Ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
-const { bookingApi } = useServices()
+const { bookingApi, roomsApi } = useServices()
 const route = useRoute();
 const router = useRouter()
 
 const booking: Ref<Booking | null> = ref(null)
-const startDate = ref('')
-const endDate = ref('')
-const available: Ref<boolean> = ref(false)
+const room: Ref<Room | null> = ref(null)
+const roomNumber = ref(0)
 
 bookingApi
   .getBookingById({ id: Number(route.params.id) })
-  .then((response) => (booking.value = response))
+  .then((response) => (booking.value = response,
+    roomsApi
+      .getRoomById({ id: response?.roomId ?? 0 })
+      .then((response) => (room.value = response))
+      .catch((error) => console.log(error))))
   .catch((error) => console.log(error))
 
 
@@ -30,15 +34,21 @@ const deleteBooking = (): void => {
 
 <template>
   <div class="row">
-    <div class="mx-auto col-8 col-md-5 col-lg-3">
-      <h3>Edit booking</h3>
+    <div class="mx-auto col-8 col-md-5 col-lg-6">
+      <h3>Booking details</h3>
       <div class="card bg-light">
         <div class="card-body">
-          <h5 class="card-title">{{ booking == null ? "Loading..." : "Room number: " + booking.roomId }}</h5>
-          <p class="card-text">Price per night: {{ booking == null ? "Loading..." : booking.price }}<br /> Start date: {{
-            booking == null ? "Loading..." : booking.startDate }} <br />
+          <h5 class="card-title"> {{ room == null ? "Loading..." : room.description }} </h5>
+          <p class="card-text">Total Price: {{ booking == null ? "Loading..." : booking.price }}<br /> {{ booking == null
+            ? "Loading..." : "Room number: " + booking.roomId }} <br />
+            Start date: {{ booking == null ? "Loading..." : booking.startDate }} <br />
             End date: {{ booking == null ? "Loading..." : booking.endDate }} <br /> Status: {{ booking == null ?
-              "Loading..." : booking.status }}</p>
+              "Loading..." : booking.status }} <br /> <br /> 
+              
+            <h5>Room info: </h5>
+            Price per night: {{ room == null ? "Loading..." : room.price }} <br />
+            Max number of
+            guests: {{ room == null ? "Loading..." : room.guestsMaxNumber }}</p>
 
           <div class="d-flex justify-content-between mt-3">
             <button type="button" @click="deleteBooking()" class="btn btn-secondary">
